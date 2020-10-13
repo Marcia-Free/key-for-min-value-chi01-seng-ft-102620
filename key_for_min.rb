9e7eec9c801e32bb16cc2a1f37fffc66019aea26
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-13 18:05:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,26 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  previous_key = ""
+  min_key = ""
+  
+  if name_hash.empty?
+    return nil
+  end
+  
+  name_hash.each do |key, value|
+    
+    if previous_key == ""
+      previous_key = name_hash[key]
+      
+    elsif name_hash[key] >= previous_key
+      previous_key = name_hash[key]
+      
+    else
+     min_key = key
+    end
+     
+  return min_key
+  end
+  
 end
