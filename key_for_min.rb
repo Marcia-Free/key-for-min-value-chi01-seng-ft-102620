9e4916b922745c3bc01d45f0e832e42f0b097b47
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-13 18:16:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,7 +12,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value, index|
     
 
-      if previous_value < value && index <=
+      if previous_value > value
       min_key = key
     end
     
